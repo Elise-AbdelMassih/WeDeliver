e39d62aee408c0986ef5a5512922758c492995ff
Elise <eliseabdmassih@gmail.com>
2024-07-17 14:40:57 +0300

    Add files via upload
    Defining a Driver class to represent a delivery driver, and a City class to represent a city in the delivery network

---
+++ # we_deliver_commit1.py
@@ -0,0 +1,11 @@
+# we_deliver_commit1.py
+
+class Driver:
+    def __init__(self, worker_id, name, start_city):
+        self.worker_id = worker_id
+        self.name = name
+        self.start_city = start_city
+
+class City:
+    def __init__(self, name):
+        self.name = name
